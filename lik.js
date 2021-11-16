448d91ea9c595d32f0bc5ea8bf225256d2842613
xyRks <52831378+xyRks@users.noreply.github.com>
2021-11-16 21:25:45 +0300

    add

--- lik.js
+++ lik.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
     $('.number-container').slick({
         arrows: true,
-        dots: true
+        dots: true,
+        adaptiveWidth: true
     });
 })
+
+
+$(document).ready(function(){
+    $('.review-container').slick({
+        arrows: true,
+        dots: true,
+        adaptiveWidth: true,
+        slidesToShow: 2
+    });
+})
+
