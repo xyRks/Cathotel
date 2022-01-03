6a603f4f59b7501e5adcee2f9449a3f64ad549c0
xyRks <52831378+xyRks@users.noreply.github.com>
2022-01-03 22:02:01 +0300

    add

---
+++ catalog/filter.js
@@ -0,0 +1,28 @@
+
+function chek() {
+    let chbox = document.getElementById('1');
+
+    if (chbox.checked ) {
+        return document.getElementById('target').style.display = "none" ;
+        }
+    else {
+       return  document.getElementById('target').style.display = "block";
+    }
+}
+
+
+
+// function check () {
+//     let chbox = document.getElementById('1');
+//     let answer = "";
+
+
+//         if (chbox.checked ) {
+//             return  answer = true;
+//             }
+//         else {
+//            return answer = false;
+//     }
+// }
+    
+// console.log(check())
