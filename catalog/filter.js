cecc892a2887638f51ba36c2fc441d3c621df638
xyRks <52831378+xyRks@users.noreply.github.com>
2022-01-08 20:37:31 +0300

    add

--- catalog/filter.js
+++ catalog/filter.js
@@ -1,36 +1,29 @@
+let articles = document.querySelectorAll('.catalog__list-card');
+let filters = document.querySelectorAll('.catalog__aside-palase');
+let filres2 = document.querySelectorAll('.catalog__aside-number');
+// for (let i; i < filter.length; i++) {
 
-function chek() {
-    let chbox = document.getElementById('1');
-    let card = document.querySelectorAll('#target')
-
-
-    
-    if (chbox.checked ) {
-        for (let key of card) {
-             key.classList.add('none')
-         }
-        }
-    else {
-        for (let key of card) {
-            key.classList.remove('none')
-         }
+// }
+for (let filter of filters) {
+filter.onchange = function () {
+    for (let article of articles) {
+      if (filter.checked && article.dataset.category !== filter.value) {
+        article.classList.add('none');
+      } else  {
+        article.classList.remove('none');
+      }
     }
+  };
 }
 
-console.log(chek())
-
-// function check () {
-//     let chbox = document.getElementById('1');
-//     let answer = "";
-
-
-//         if (chbox.checked ) {
-//             return  answer = true;
-//             }
-//         else {
-//            return answer = false;
-//     }
-// }
-    
-// console.log(check())
- 
+for (let filter2 of filres2) {
+    filter2.onchange = function () {
+        for (let article of articles) {
+          if (filter2.checked && article.dataset.palace != filter2.value) {
+            article.classList.add('none'); 
+          } else  {
+            article.classList.remove('none');
+          }
+        }
+      };
+    }
